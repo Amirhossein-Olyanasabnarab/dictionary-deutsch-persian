f40aff95932df7ba7925e11893c10e42ef82448f
amirholya01 <amirholya01@gmail.com>
2025-12-03 09:09:21 +0330

    Using jpa annotation for BaseWord class

--- src/main/java/com/dev/dictionary/model/entity/BaseWord.java
+++ src/main/java/com/dev/dictionary/model/entity/BaseWord.java
@@ -1,16 +1,53 @@
 package com.dev.dictionary.model.entity;
 
 import com.dev.dictionary.model.enums.WordType;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
 public abstract class BaseWord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 100)
     private String german;
+
+    @Column(nullable = false, length = 100)
     private String persian;
+
+    @Column(nullable = false, length = 500)
     private String exampleSentenceGermany;
+
+    @Column(nullable = false, length = 500)
     private String exampleSentencePersian;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private WordType wordType;
+
+    @CreatedDate
     private LocalDateTime createTime;
+
+    @LastModifiedDate
     private LocalDateTime updateTime;
 }
